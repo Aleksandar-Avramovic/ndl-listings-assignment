4729f10778a6390977a795f59de8d1566de63d4e
Aleksandar Avramovic <aleksandar.avramovic@forwardslashny.com>
2024-08-08 16:04:18 +0200

    Started working on filter popup

--- src/js/mobileFilterHandler.js
+++ src/js/mobileFilterHandler.js
@@ -2,4 +2,5 @@ const filterIcon = document.querySelector(".filter__mobile");
 
 filterIcon.addEventListener("click", () => {
   document.querySelector(".property-filters").classList.add("open");
+  disableScroll();
 });
