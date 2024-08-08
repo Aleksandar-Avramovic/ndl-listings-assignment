09dca4bf0445efbaf0621eda4112229ddfd66ea1
Aleksandar Avramovic <aleksandar.avramovic@forwardslashny.com>
2024-08-08 14:42:11 +0200

    Added overlay for filter on 992px

---
+++ src/js/mobileFilterHandler.js
@@ -0,0 +1,5 @@
+const filterIcon = document.querySelector(".filter__mobile");
+
+filterIcon.addEventListener("click", () => {
+  document.querySelector(".property-filters").classList.add("open");
+});
