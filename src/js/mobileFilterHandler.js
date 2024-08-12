052044fe3c02f923c045dbc26ac0eaa904c227fc
Aleksandar Avramovic <aleksandar.avramovic@forwardslashny.com>
2024-08-12 23:33:02 +0200

    Added popup close icon

--- src/js/mobileFilterHandler.js
+++ src/js/mobileFilterHandler.js
@@ -1,6 +1,14 @@
 const filterIcon = document.querySelector(".filter__mobile");
+const popupCloseIcon = document.querySelector(".popup__close");
 
 filterIcon.addEventListener("click", () => {
   document.querySelector(".property-filters").classList.add("open");
+  popupCloseIcon.classList.add("open");
   disableScroll();
+});
+
+popupCloseIcon.addEventListener("click", () => {
+  document.querySelector(".property-filters").classList.remove("open");
+  popupCloseIcon.classList.remove("open");
+  enableScroll();
 });
